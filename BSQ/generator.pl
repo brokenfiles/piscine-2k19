f84a79a5bd91949cf48b1e23369f0510bb0ac8e2
brokenfiles <louis.nevysup@gmail.com>
2019-09-18 10:49:27 +0200

    adding new generator.pl (can change the void, full and obstacle char) (not optimised for the moment)

--- BSQ/generator.pl
+++ BSQ/generator.pl
@@ -13,16 +13,39 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-die "program x y density" unless (scalar(@ARGV) == 3);
-my ($x, $y, $density, $void, $obs, $full) = @ARGV;
-print "$yox\n";
-for (my $i = 0; $i < $y; $i++) {
-    for (my $j = 0; $j < $x; $j++) {
-           if (int(rand($y) * 2) < $density) {
-           print "o";
-           }
-           else {
-           print "."; }
-           }
-           print "\n";
-           }
+die "program x y density [void, obs, full]" unless (scalar(@ARGV) == 6 || scalar(@ARGV) == 3);
+if (scalar(@ARGV) == 3)
+{
+	my ($x, $y, $density) = @ARGV;
+	print "$y.ox\n";
+		for (my $i = 0; $i < $y; $i++) {
+  		  for (my $j = 0; $j < $x; $j++) {
+   	        if (int(rand($y) * 2) < $density) 
+			   {
+					print "o";
+   	    	    }
+   	        else 
+			   {
+					print "."; }
+   		        }
+		print "\n"; 
+	}
+}
+else
+{
+	my ($x, $y, $density, $void, $obs, $full) = @ARGV;
+	print "$y$void$obs$full\n";
+		for (my $i = 0; $i < $y; $i++) {
+  		  for (my $j = 0; $j < $x; $j++) {
+   	        if (int(rand($y) * 2) < $density) 
+			   {
+					print "$obs";
+   		        }
+   	        else 
+			   {
+					print "$void"; }
+   		        }
+		print "\n"; 
+	}
+}
+
